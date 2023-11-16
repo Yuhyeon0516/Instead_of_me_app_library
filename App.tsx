c520b42403e27042909adda88d5a4fc0f6a7d312
Yuhyeon0516 <rladbgus0516@naver.com>
2023-11-16 21:24:56 +0900

    Created selcet payment product screen.

--- App.tsx
+++ App.tsx
@@ -1,11 +1,19 @@
-import {Box, NativeBaseProvider} from '@yuhyeonkim/native-base';
-import React from 'react';
+import React from "react";
+import Payment from "./src/select_payment_product";
+import {GluestackUIProvider} from "@gluestack-ui/themed";
+import {config} from "@gluestack-ui/config";
+import {ToastProvider} from "@gluestack-ui/toast";
+import {OverlayProvider} from "@gluestack-ui/overlay";
 
 function App(): JSX.Element {
     return (
-        <NativeBaseProvider>
-            <Box />
-        </NativeBaseProvider>
+        <GluestackUIProvider config={config}>
+            <ToastProvider>
+                <OverlayProvider>
+                    <Payment />
+                </OverlayProvider>
+            </ToastProvider>
+        </GluestackUIProvider>
     );
 }
 
