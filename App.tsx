96c3f6944537abd817a843dbccb8fbfe8ec06933
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-19 20:39:35 +0900

    Created interest popup screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import AddExperience from "./src/add_experience";
+import InterestPopup from "./src/interest_popup";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <AddExperience />
+                        <InterestPopup />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
