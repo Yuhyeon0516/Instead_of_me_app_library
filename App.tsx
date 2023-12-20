ad7ad956a22aeb3ddb9ec9d742f1f7048af2507f
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-20 18:45:54 +0900

    Created job listings screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import NutritionalSupplementRecommendation from "./src/nutritional_supplement_recommendation";
+import JobListings from "./src/job_listings";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <NutritionalSupplementRecommendation />
+                        <JobListings />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
