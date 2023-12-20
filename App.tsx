720c4508131a7bdb56d38773cc16cbba21e26b16
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-20 20:23:47 +0900

    Created register notice screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import JobListings from "./src/job_listings";
+import RegisterNotice from "./src/register_notice";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <JobListings />
+                        <RegisterNotice />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
