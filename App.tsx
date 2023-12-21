3dd0fd6814bd561729b81ade059d15fced38a709
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-21 21:49:19 +0900

    Created search market screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import CustomAnalytics from "./src/custom_analytics";
+import SearchMarket from "./src/search_market";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <CustomAnalytics />
+                        <SearchMarket />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
