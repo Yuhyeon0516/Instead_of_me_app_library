d46f4b4a031801c72ef1f04939aa0144cc7778da
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-21 20:05:53 +0900

    Created splash loading screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import SimpleLogin from "./src/simple_login";
+import SplashLoading from "./src/splash_loading";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <SimpleLogin />
+                        <SplashLoading />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
