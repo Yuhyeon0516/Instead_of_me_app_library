aa963b166f8d1da4c5cbf3eebd05a42cdbb0ea84
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-21 14:08:54 +0900

    Created talent mangement screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import BusinessPopup from "./src/business_popup";
+import TalentManagement from "./src/talent_management";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <BusinessPopup />
+                        <TalentManagement />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
