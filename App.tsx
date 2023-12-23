b7b87032b4c0e13b580267bcc762d62ffa160638
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-23 21:16:43 +0900

    Created inquiry list screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import SelectModifyMarketInformation from "./src/select_modify_market_information";
+import InquiryList from "./src/inquiry_list";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <SelectModifyMarketInformation />
+                        <InquiryList />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
