525857fa71475a4bb7ec400308b463fcebf19fed
Yuhyeon0516 <rladbgus0516@naver.com>
2023-12-23 23:43:46 +0900

    Created delivery home screen.

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import {config} from "@gluestack-ui/config";
 import {ToastProvider} from "@gluestack-ui/toast";
 import {OverlayProvider} from "@gluestack-ui/overlay";
 import {SafeAreaProvider} from "react-native-safe-area-context";
-import DeliveryStoreDetail from "./src/delivery_store_detail";
+import DeliveryHome from "./src/delivery_home";
 
 function App(): JSX.Element {
     return (
@@ -12,7 +12,7 @@ function App(): JSX.Element {
             <SafeAreaProvider style={{backgroundColor: "white"}}>
                 <ToastProvider>
                     <OverlayProvider>
-                        <DeliveryStoreDetail />
+                        <DeliveryHome />
                     </OverlayProvider>
                 </ToastProvider>
             </SafeAreaProvider>
